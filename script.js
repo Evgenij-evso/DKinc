65708b83f4eb2796be2c5e09d64d96e048e2b6f3
Evgenij <t89258000005@gmail.com>
2023-08-15 18:29:04 +0300

    add twitch_rights,merch

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 let menu_link = document.querySelectorAll('.menu_link');
 let ac = document.querySelectorAll('.style_activities');
+let card_merch = document.querySelectorAll('.card_merch')
 
 let previes_text = document.querySelector('.previes_text')
 let span_previews_text = document.querySelector('.span_previews_text');
+
+let merch = document.getElementById('merch')
+let Twitch_rights = document.getElementById('Twitch_rights')
+
+let anime_twitch = document.querySelectorAll('.anime_twitch')
+$(merch).click(function(){
+    window.scrollBy({
+        top: merch,
+        behavior: "smooth"
+    });
+})
+$(Twitch_rights).click(function(){
+    window.scrollBy({
+        top: Twitch_rights,
+        behavior: "smooth"
+    });
+})
 
 for(let i = 0; menu_link.length > i; i++){
     $(menu_link[i]).mouseenter(function(){
@@ -36,7 +54,7 @@ for(let i = 0; ac.length > i; i++){
         }
         else if(i == 3){
 
-            span_previews_text.textContent = 'Пародийные треки, Дисы и треки написаные в рубрике, на канале Дани Кашена - DkIncc'
+            span_previews_text.textContent = 'Пародийные треки, Дисы и треки написаные в рубрике, на канале Дани Кашина - DkIncc'
         }
 
         anime({
@@ -52,6 +70,44 @@ for(let i = 0; ac.length > i; i++){
             scale: 1,
             duration: 100,
             easing: 'linear',
+        })
+    })
+}
+for(let i = 0; card_merch.length > i; i++){
+    $(card_merch[i]).mouseenter(function(){
+        anime({
+            targets: this,
+            translateY: -10,
+            borderColor: '#cf3ad4',
+            duration: 200,
+            easing: 'linear'
+        })
+    }).mouseleave(function(){
+        anime({
+            targets: this,
+            translateY: 0,
+            borderColor: '#cf3ad400',
+            duration: 200,
+            easing: 'linear'
+        })
+    })
+}
+for(let i = 0; anime_twitch.length > i; i++){
+    $(anime_twitch[i]).mouseenter(function(){
+        anime({
+            targets: this,
+            scale: 1.03,
+            borderColor: '#cf3ad4',
+            duration: 100,
+            easing: 'linear'
+        })
+    }).mouseleave(function(){
+        anime({
+            targets: this,
+            scale: 1,
+            borderColor: '#cf3ad400',
+            duration: 100,
+            easing: 'linear'
         })
     })
 }
